59d063bb75b72b97b6cd45c44c5b78b7da371475
dddasd <asdasdasd>
2021-08-26 08:57:36 +0200

    Version 1

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -16,69 +16,114 @@ class ViewController: UIViewController {
     var x = 0.0;
     var y = 0.0;
     var sum = 0.0;
+    var op = "+"
+    
+    
     @IBAction func Num7(_ sender: UIButton) {
         TextField.text = TextField.text! + "7"
     }
     
     @IBOutlet weak var TextField: UILabel!
-    @IBAction func Num8(_ sender: Any) {
+    @IBAction func Num8(_ sender: UIButton) {
         TextField.text = TextField.text! + "8"
 }
 
 
-    @IBAction func Num9(_ sender: Any) {
+    @IBAction func Num9(_ sender: UIButton) {
         TextField.text = TextField.text! + "9"}
     
 
-    @IBAction func Num4(_ sender: Any) {TextField.text = TextField.text! + "4"
+    
+    @IBAction func Num4(_ sender: UIButton) {TextField.text = TextField.text! + "4"
     }
     
 
-    @IBAction func Num5(_ sender: Any) {
+    @IBAction func Num5(_ sender: UIButton) {
         TextField.text = TextField.text! + "5"}
     
 
-    @IBAction func Num6(_ sender: Any) {TextField.text = TextField.text! + "6"
+    @IBAction func Num6(_ sender: UIButton) {TextField.text = TextField.text! + "6"
     }
     
     
     
-    @IBAction func Num1(_ sender: Any) {
-        TextField.text = TextField.text! + "1"}
-    
-    @IBAction func Num2(_ sender: Any) {TextField.text = TextField.text! + "2"
+    @IBAction func Num1(_ sender: UIButton) {
+        TextField.text = TextField.text! + "1"
     }
     
-    @IBAction func Num3(_ sender: Any) {
+
+    
+    @IBAction func Num2(_ sender: UIButton) {TextField.text = TextField.text! + "2"
+    }
+    
+    @IBAction func Num3(_ sender: UIButton) {
         TextField.text = TextField.text! + "3"}
     
     
-    @IBAction func Num0(_ sender: Any) {TextField.text = TextField.text! + "0"
+    @IBAction func Num0(_ sender: UIButton) {TextField.text = TextField.text! + "0"
     }
     
     
-    @IBAction func Multiply(_ sender: Any) {
+    @IBAction func Multiply(_ sender: UIButton) {
+        op = "*"
+        x = Double(TextField.text!)!
+        clearText()
     }
     
     
     
-    @IBAction func Divide(_ sender: Any) {
+    @IBAction func Divide(_ sender: UIButton) {
+        op = "/"
+        x = Double(TextField.text!)!
+        clearText()
     }
     
     
-    @IBAction func Add(_ sender: Any) {
+    @IBAction func Add(_ sender: UIButton) {
+        op = "+"
+        x = Double(TextField.text!)!
+        clearText()
+
     }
     
-    @IBAction func Equals(_ sender: Any) {
-    }
     
-    @IBAction func Subtract(_ sender: Any) {
+    
+    @IBAction func Subtract(_ sender: UIButton) {
+        op = "-"
+        x = Double(TextField.text!)!
+        clearText()
     }
     
     
     @IBOutlet weak var Screen: UILabel!
     
+    @IBAction func buttonClear(Sender: UIButton){
+          clearText()
+      }
+      
+      func clearText() {
+          TextField.text = ""
+      }
     
-    
+    @IBAction func Equals(_ sender: UIButton) {
+
+        y = Double(TextField.text!)!
+    switch op{
+         case "+":
+             sum = x+y
+             TextField.text = String(sum)
+    case "-":
+        sum = x-y
+        TextField.text = String(sum)
+    case "*":
+        sum = x*y
+        TextField.text = String(sum)
+    case "/":
+        sum = x/y
+        TextField.text = String(sum)
+         default:
+             TextField.text = "ERROR"
+             
+    }}
 }
 
